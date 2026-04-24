6baed9d316de5bf9a634cc0953e4f8446f929029
Oleg Gonchar <102986290+oleg44444@users.noreply.github.com>
2026-04-24 12:52:11 +0300

    init bot

--- config/config.js
+++ config/config.js
@@ -2,20 +2,20 @@ require('dotenv').config();
 
 module.exports = {
     symbol: 'SOLUSDT',
-    timeframe: '5m',
-    tradeAmount: 5,
+    timeframe: '15m',
+    tradeAmount: 1,
     leverage: 20,
     updateInterval: 30000,
     maxRequestsPerMinute: 50,
     binance: {
-        apiKey: process.env.BINANCE_API_KEY,
-        apiSecret: process.env.BINANCE_API_SECRET,
-        testnet: true
+        apiKey: process.env.BINANCE_API_KEY_REAL,
+        apiSecret: process.env.BINANCE_API_SECRET_REAL,
+        testnet: false
     },
     DCA_CONFIG: {
-        MAX_DCA_COUNT: 10,
-        STEP_PERCENT: 1, // наприклад, 1.5% відстань між докупками
-        MULTIPLIER: 0.8    // коефіцієнт збільшення кожної наступної докупки
+        MAX_DCA_COUNT: 5,
+        STEP_PERCENT: 1, 
+        MULTIPLIER: 0.8   
       },
     telegram: {
         token: process.env.TELEGRAM_BOT_TOKEN,
